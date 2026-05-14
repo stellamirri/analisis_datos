2eafa8ca94fb41282ab5cb8daa992f5a02378110
Killian <killian.bujon@etu.minesparis.psl.eu>
2026-05-14 12:36:29 +0200

    API

---
+++ Grupo1_2026/API.py
@@ -0,0 +1,38 @@
+from entsoe import EntsoePandasClient
+import pandas as pd
+#DL the module entsoe 
+
+client = EntsoePandasClient(
+    api_key='5bcf63bf-87d7-4aa5-8bfb-040cbc4c4af0'
+)
+
+
+##This is my token 
+start = pd.Timestamp(
+    '2025-01-01',
+    tz='Europe/Brussels'
+)
+
+end = pd.Timestamp(
+    '2025-03-01',
+    tz='Europe/Brussels'
+)
+
+es = client.query_day_ahead_prices(
+    'ES',
+    start=start,
+    end=end
+)
+
+fr = client.query_day_ahead_prices(
+    'FR',
+    start=start,
+    end=end
+)
+
+df = pd.DataFrame({
+    'Spain': es,
+    'France': fr
+})
+
+print(df.head())

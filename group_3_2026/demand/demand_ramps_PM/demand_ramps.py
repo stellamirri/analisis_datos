fb3a8298a4f48dc8acf71ce3506cc9fc4e09d63c
Pelayo <pelayinco@gmail.com>
2026-05-21 19:02:41 +0200

    i load data clean

--- group_3_2026/demand/demand_ramps_PM/demand_ramps.py
+++ group_3_2026/demand/demand_ramps_PM/demand_ramps.py
@@ -6,4 +6,3 @@ df_fr = pd.read_csv("data\\processed\\france_cleaned.csv")
 df_de = pd.read_csv("data\\processed\\germany_cleaned.csv")
 
 # creating a new column for the date and time
-df_fr["datetime"] = pd.to_datetime(df_fr["date"] + " " + df _fr["time"])

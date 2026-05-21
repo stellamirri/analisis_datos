199e04f0abdada046c73aa7e6ce048606bb3bdf4
Pelayo <pelayinco@gmail.com>
2026-05-21 18:59:08 +0200

    I load clean data

--- group_3_2026/demand/demand_ramps_PM/demand_ramps.py
+++ group_3_2026/demand/demand_ramps_PM/demand_ramps.py
@@ -4,4 +4,6 @@ import pandas as pd
 # loading the cleaned data for France, Germany, and Spain
 df_fr = pd.read_csv("data\\processed\\france_cleaned.csv")
 df_de = pd.read_csv("data\\processed\\germany_cleaned.csv")
-df_es = pd.read_csv("data\\processed\\spain_cleaned.csv")
+
+# creating a new column for the date and time
+df_fr["datetime"] = pd.to_datetime(df_fr["date"] + " " + df _fr["time"])

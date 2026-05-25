2c640bb074064e8bcd7157af6e4b05d27da4f650
xishao <xi.shao@alumnos.upm.es>
2026-05-25 03:12:52 +0200

    Revert "Residual load "

--- group_3_2026/demand/demand_ramps_PM/demand_ramps.py
+++ group_3_2026/demand/demand_ramps_PM/demand_ramps.py
@@ -1,8 +1,1 @@
 # prueba 
-import pandas as pd
-
-# loading the cleaned data for France, Germany, and Spain
-df_fr = pd.read_csv("data\\processed\\france_cleaned.csv")
-df_de = pd.read_csv("data\\processed\\germany_cleaned.csv")
-
-# creating a new column for the date and time
--- group_3_2026/demand/variability AP/variability.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-# loading the cleaned data for France, Germany, and Spain
-df_fr = pd.read_csv("data\\processed\\france_cleaned.csv")
-df_de = pd.read_csv("data\\processed\\germany_cleaned.csv")
-df_es = pd.read_csv("data\\processed\\spain_cleaned.csv")
